7de939dea2ce85853cfb1ae055aa5885d9dad4b8
lanchuan lin <lin.lan@northeastern.edu>
2023-12-20 17:39:10 -0800

    feat: add abstract cache and shared array

--- src/main/java/com/northeastern/edu/simpledb/backend/common/SubArray.java
+++ src/main/java/com/northeastern/edu/simpledb/backend/common/SubArray.java
@@ -1,2 +1,13 @@
-package com.northeastern.edu.simpledb.backend.common;public class SubArray {
+package com.northeastern.edu.simpledb.backend.common;
+
+public class SubArray {
+    public byte[] raw;
+    public int start;
+    public int end;
+
+    public SubArray(byte[] raw, int start, int end) {
+        this.raw = raw;
+        this.start = start;
+        this.end = end;
+    }
 }
