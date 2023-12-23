9a3d51e4fe8464f3de71740c323e594eaab05f59
lanchuan lin <lin.lan@northeastern.edu>
2023-12-23 09:44:12 -0800

    feat: add page

---
+++ src/main/java/com/northeastern/edu/simpledb/backend/dm/page/Page.java
@@ -0,0 +1,58 @@
+package com.northeastern.edu.simpledb.backend.dm.page;
+
+import com.northeastern.edu.simpledb.backend.dm.cache.PageCache;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Page extends AbstractPage {
+
+    private int pageNumber;
+    private byte[] data;
+    private boolean dirty;
+    private Lock lock;
+    private PageCache pc;
+
+
+    public Page(int pageNumber, byte[] data, PageCache pc) {
+        this.pageNumber = pageNumber;
+        this.data = data;
+        this.pc = pc;
+        this.lock = new ReentrantLock();
+    }
+
+    @Override
+    public void lock() {
+        lock.lock();
+    }
+
+    @Override
+    public void unlock() {
+        lock.unlock();
+    }
+
+    @Override
+    public void release() {
+        pc.release(this);
+    }
+
+    @Override
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
+    }
+
+    @Override
+    public boolean isDirty() {
+        return this.dirty;
+    }
+
+    @Override
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    @Override
+    public byte[] getData() {
+        return data;
+    }
+}
