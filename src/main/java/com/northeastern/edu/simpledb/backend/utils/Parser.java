2c65348469325c6e56178c415038ccb43040546c
lanchuan lin <lin.lan@northeastern.edu>
2023-12-23 09:45:32 -0800

    feat: add short parser

--- src/main/java/com/northeastern/edu/simpledb/backend/utils/Parser.java
+++ src/main/java/com/northeastern/edu/simpledb/backend/utils/Parser.java
@@ -4,12 +4,21 @@ import java.nio.ByteBuffer;
 
 public class Parser {
 
-    public static long parseLong(byte[] buf) {
-        ByteBuffer buffer = ByteBuffer.wrap(buf, 0, 8);
-        return buffer.getLong();
+    public static long parseLong(byte[] bytes) {
+        ByteBuffer buf = ByteBuffer.wrap(bytes, 0, 8);
+        return buf.getLong();
     }
 
     public static byte[] long2Byte(long value) {
         return ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(value).array();
+    }
+
+    public static short parseShort(byte[] bytes) {
+        ByteBuffer buf = ByteBuffer.wrap(bytes, 0, 2);
+        return buf.getShort();
+    }
+
+    public static byte[] short2Byte(short value) {
+        return ByteBuffer.allocate(Short.SIZE / Byte.SIZE).putShort(value).array();
     }
 }
