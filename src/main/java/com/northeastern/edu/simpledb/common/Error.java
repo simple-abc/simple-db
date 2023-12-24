bed150136ba204b00fc9504f8a86f5fab6a0d596
lanchuan lin <lin.lan@northeastern.edu>
2023-12-23 19:06:19 -0800

    feat: add more custom error

--- src/main/java/com/northeastern/edu/simpledb/common/Error.java
+++ src/main/java/com/northeastern/edu/simpledb/common/Error.java
@@ -4,6 +4,7 @@ public class Error {
 
     // common
     public static final Exception FileNotExistsException = new RuntimeException("File does not exists!");
+    public static final Exception FileExistsException = new RuntimeException("File already exists!");
     public static final Exception FileCannotRWException = new RuntimeException("File cannot read or write!");
 
     public static final Exception CacheFullException = new RuntimeException("Cache is full!");
@@ -11,6 +12,7 @@ public class Error {
 
     // dm
     public static final Exception MemTooSmallException = new RuntimeException("Memory too small!");
+    public static final Exception BadLogFileException = new RuntimeException("Bad log file!");
 
     // tm
     public static final Exception BadXIDFileException = new RuntimeException("Bad XID file!");
