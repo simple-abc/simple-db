d005cb1527d7e177e9f6d587a81c4ded3b88ec3f
lan lin <lin.lan@northeastern.edu>
2024-01-05 17:25:33 -0800

    fix: null pointer exception

--- src/main/java/com/northeastern/edu/simpledb/backend/vm/Visibility.java
+++ src/main/java/com/northeastern/edu/simpledb/backend/vm/Visibility.java
@@ -4,18 +4,23 @@ import com.northeastern.edu.simpledb.backend.tm.TransactionManager;
 
 public class Visibility {
 
+    /**
+     * version skip happens when e has been updated by t1 from v0
+     * to v1, but t1 can't be seen by t2, and t2 try to update e,
+     * the version of it will transfer from v0 to v2, skipping v1
+     */
     public static boolean isVersionSkip(TransactionManager tm, Transaction t, Entry e) {
         long xmax = e.getXmax();
         if (t.level == 0) return false;
         else return tm.isCommitted(xmax) && (xmax > t.xid || t.isInSnapShot(xmax));
     }
 
-    public static boolean isVisible(TransactionManager tm, Transaction t, Entry e) {
+    // determine if record(e) is visible to transaction(t) based on isolation level
+    protected static boolean isVisible(TransactionManager tm, Transaction t, Entry e) {
         if(t.level == 0) return readCommitted(tm, t, e);
         else return repeatableRead(tm, t, e);
     }
 
-    // determine if record(e) is visible to transaction(t)
     private static boolean readCommitted(TransactionManager tm, Transaction t, Entry e) {
         long xid = t.xid;
         long xmin = e.getXmin();
